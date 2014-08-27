afe7285e33d151ce2ba046a6cda0beca32b355b0
Tristan Siegel <tristan.t.siegel@gmail.com>
2014-08-27 11:19:10 -0400

    removes package installs and brew

--- bootstrap.sh
+++ bootstrap.sh
@@ -29,60 +29,6 @@ function copyBashProfile {
   curl "https://raw.githubusercontent.com/flatiron-school/dotfiles/master/bash_profile" -o ".bash_profile"
 }
 
-function getCommandLineTools {
-  echo 'Downloading command line tools...'
-  cd ~
-
-  if [ -f cli_tools.dmg ]; then
-    rm cli_tools.dmg
-  fi
-
-  curl "http://flatiron-school.s3.amazonaws.com/software/command_line_tools_os_x_mavericks_for_xcode__late_october_2013.dmg" -o "cli_tools.dmg"
-}
-
-function installCommandLineTools {
-  echo 'Installing command line tools...'
-  cd ~
-
-  hdiutil attach cli_tools.dmg
-  sudo installer -pkg "/Volumes/Command Line Developer Tools/Command Line Tools (OS X 10.9).pkg" -target "/Volumes/Macintosh HD"
-  hdiutil detach "/Volumes/Command Line Developer Tools"
-  rm cli_tools.dmg
-}
-
-function installHomebrew {
-  echo 'Installing Homebrew...'
-  cd ~
-
-  ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
-}
-
-function checkForAndInstallHomebrew {
-  echo 'Checking if Homebrew is installed...'
-  cd ~
-  
-  if ! type brew 2&> /dev/null; then
-    installHomebrew
-  fi
-}
-
-function installGit {
-  echo 'Installing Git...'
-  cd ~
-  source $HOME/.bash_profile
-  
-  checkForAndInstallHomebrew
-  brew install git
-}
-
-function installSqlite {
-  echo 'Installing SQlite3...'
-  cd ~
-
-  checkForAndInstallHomebrew
-  brew install sqlite3
-}
-
 function installRVM {
   echo 'Installing RVM and Ruby 2.1.2...'
   cd ~
@@ -91,45 +37,6 @@ function installRVM {
   source $HOME/.bash_profile
 
   rvm use 2.1.2 --default
-}
-
-function getSublime {
-  echo 'Downloading SublimeText 2.0...'
-  cd ~
-  
-  if [ -f sublime.dmg ]; then
-    rm sublime.dmg
-  fi
-
-  curl "http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%202.0.2.dmg" -o "sublime.dmg"
-}
-
-function installSublime {
-  echo 'Installing SublimeText 2.0...'
-  cd ~
-
-  hdiutil attach sublime.dmg
-  cp -r "/Volumes/Sublime Text 2/Sublime Text 2.app" "/Applications/Sublime Text 2.app"
-  hdiutil detach "/Volumes/Sublime Text 2"
-
-  rm sublime.dmg
-  
-  checkForAndInstallHomebrew
-  sudo ln -s "/Applications/Sublime Text 2.app/Contents/SharedSupport/bin/subl" /usr/local/bin
-  open "/Applications/Sublime Text 2.app" && sleep 3 && killall "Sublime Text 2"
-  
-  cd "$HOME/Library/Application Support/Sublime Text 2/Installed Packages"
-  curl "https://sublime.wbond.net/Package%20Control.sublime-package" -o "Package Control.sublime-package"
-
-  cd "$HOME/Library/Application Support/Sublime Text 2/Packages/Color Scheme - Default"
-  curl "http://flatironschool.s3.amazonaws.com/curriculum/resources/environment/themes/Solarized%20Flatiron.zip" -o "Solarized Flatiron.zip"
-  tar -zxvf "Solarized Flatiron.zip"
-  rm "Solarized Flatiron.zip"
-  
-  cd "$HOME/Library/Application Support/Sublime Text 2/Packages/Default"
-  sed -i '' "s/\"color_scheme\": \"Packages\/Color Scheme - Default\/Monokai.tmTheme\",/\"color_scheme\": \"Packages\/Color Scheme - Default\/Solarized Light (Flatiron).tmTheme\",/g" Preferences.sublime-settings
-  sed -i '' "s/\"tab_size\": 4,/\"tab_size\": 2,/g" Preferences.sublime-settings
-  sed -i '' "s/\"translate_tabs_to_spaces\": false,/\"translate_tabs_to_spaces\": true,/g" Preferences.sublime-settings
 }
 
 function getGitconfig {
@@ -206,23 +113,41 @@ function setupDirStructure {
   mkdir -p Development/code
 }
 
+# This should be put in the ubuntu installer when it adds sublime
+# function setsUpSublimePreferences {
+#   echo 'Setting Up SublimeText 3.0...'
+#   cd ~
+
+
+#   # cp -r "/Volumes/Sublime Text 2/Sublime Text 2.app" "/Applications/Sublime Text 2.app"
+#   sudo ln -s "/Applications/Sublime Text 2.app/Contents/SharedSupport/bin/subl" /usr/local/bin
+#   open "/Applications/Sublime Text 2.app" && sleep 3 && killall "Sublime Text 2"
+  
+#   cd "$HOME/Library/Application Support/Sublime Text 2/Installed Packages"
+#   curl "https://sublime.wbond.net/Package%20Control.sublime-package" -o "Package Control.sublime-package"
+
+#   cd "$HOME/Library/Application Support/Sublime Text 2/Packages/Color Scheme - Default"
+#   curl "http://flatironschool.s3.amazonaws.com/curriculum/resources/environment/themes/Solarized%20Flatiron.zip" -o "Solarized Flatiron.zip"
+#   tar -zxvf "Solarized Flatiron.zip"
+#   rm "Solarized Flatiron.zip"
+  
+#   cd "$HOME/Library/Application Support/Sublime Text 2/Packages/Default"
+#   sed -i '' "s/\"color_scheme\": \"Packages\/Color Scheme - Default\/Monokai.tmTheme\",/\"color_scheme\": \"Packages\/Color Scheme - Default\/Solarized Light (Flatiron).tmTheme\",/g" Preferences.sublime-settings
+#   sed -i '' "s/\"tab_size\": 4,/\"tab_size\": 2,/g" Preferences.sublime-settings
+#   sed -i '' "s/\"translate_tabs_to_spaces\": false,/\"translate_tabs_to_spaces\": true,/g" Preferences.sublime-settings
+# }
+
 function completeSetup {
   echo "Done!"
 }
 
 editSudoers
 copyBashProfile
-getCommandLineTools
-installCommandLineTools
-checkFofAndInstallHomebrew
-installGit
-installSqlite
 installRVM
-getSublime
-installSublime
 getGitconfig
 setupGemrc
 getIrbrc
 setupDirStructure
+# setsUpSublimePreferences
 restoreSudoers
 completeSetup
