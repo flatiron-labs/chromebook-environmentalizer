e65ca405439190edeea567d8ebcb7adb43789ed5
Tristan Siegel <tristan.t.siegel@gmail.com>
2014-09-04 10:22:27 -0400

    adds extra brackets for if statement that breaks

--- bootstrap.sh
+++ bootstrap.sh
@@ -198,7 +198,7 @@ function setupPostgresUser {
 }
 
 function deactivateChromebookEnvironmentalizer {
-  if [ -d /etc/chromebook-environmentalizer/ && ! -f /etc/chromebook-environmentalizer/.completed ]
+  if [[ -d /etc/chromebook-environmentalizer/ && ! -f /etc/chromebook-environmentalizer/.completed ]]
     then
     sudo touch /etc/chromebook-environmentalizer/.completed
     sudo echo -e 'Hidden=true' >> /usr/share/upstart/xdg/autostart/chromebook-environmentalizer.desktop
